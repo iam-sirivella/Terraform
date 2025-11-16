c9bf187cf5cd87aa3dba19d678d6b1857fb8b45f
HariKrishna <harikrishna7408@gmail.com>
2025-11-16 20:45:09 +0530

    new

--- sample/main.tf
+++ sample/main.tf
@@ -1,9 +1,16 @@
+variable "type"{
+    type = string
+    default="t3.micro"
+}
 provider "aws"{
     region="us-east-1"
 }
 resource "aws_instance" "sample" {
     ami="ami-0ecb62995f68bb549"
     key_name="Hari-Key-pair"
-    instance_type="t3.micro"
+    instance_type=var.type
     subnet_id="subnet-05e5001d7f1539072"
+}
+output "example_output"{
+    value =resource.aws_instance.sample.ami
 }
