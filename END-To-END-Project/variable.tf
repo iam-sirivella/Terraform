5a7da8c3f1bf5894bf006200d0fe021d2b1e9bc5
HariKrishna <harikrishna7408@gmail.com>
2025-11-19 21:34:30 +0530

    new

---
+++ END-To-END-Project/variable.tf
@@ -0,0 +1,12 @@
+variable "ami_val" {
+    description="ami values"  
+}
+variable "instance_type" {
+    description = "instance type"  
+}
+variable "subnet_id"{
+    description = "subnet value"
+}
+variable "key_val" {
+    description = "key value pair"
+}
