8ef03b4b2844619bd377dd41c0922b31e7016c7d
HariKrishna <harikrishna7408@gmail.com>
2025-11-19 21:33:52 +0530

    new

--- main.tf
+++
@@ -1,8 +0,0 @@
-module "ec2_instance" {
-  source = "./modules/ec2"
-  ami_val           = var.ami_val
-  subnet_id     = var.subnet_id
-  key_val      = var.key_val
-  instance_type = var.instance_type
-  
-}
